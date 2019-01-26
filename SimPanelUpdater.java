0f6d4291a850c94331834bdb6fbb641b6d966c4a
luisacabs <luisacabs@gmail.com>
2019-01-26 15:38:32 +0000

    mais recent

---
+++ SimPanelUpdater.java
@@ -0,0 +1,37 @@
+package um.simulator.visualization;
+
+/**
+ * This class updates the simPanel automatically and periodically.
+ * @author ajcmoreira
+ * @version 2.2
+ * @date 20.04.2015
+ */
+public class SimPanelUpdater extends Thread {
+    int pace;
+    
+    /**
+     * Constructor.
+     * @param pace The time interval between updates, in milliseconds.
+     */
+    public SimPanelUpdater(int pace) {
+        this.pace = pace;
+    }
+    
+    /**
+     * Update the GUI periodically.
+     */
+    @Override
+    public void run() {
+        while (true) {
+            //update the simPanel
+            if (SimScope.keepUpdating)
+                SimScope.updateAll();
+            try {
+                //pause for a while
+                Thread.sleep(pace);
+            } catch (InterruptedException ex) {
+                System.err.println("Error while updating the GUI: " + ex.getMessage());
+            }
+        }
+    }
+}
